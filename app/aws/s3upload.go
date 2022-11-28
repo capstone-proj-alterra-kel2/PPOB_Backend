edeb1fa8f4cf9d02b3df634bd546da8dec67db6b
Zenk41 <ardhidhani@gmail.com>
2022-11-28 07:29:30 +0700

    fix(s3) : change bucket name to changeable through env

--- app/aws/s3upload.go
+++ app/aws/s3upload.go
@@ -16,6 +16,7 @@ func UploadToS3(c echo.Context, filename string, src multipart.File) (string, er
 	SECRET_KEY := util.GetEnv("AWS_S3_BUCKET_SECRET_KEY")
 	KEY_ID := util.GetEnv("AWS_S3_BUCKET_KEY_ID")
 	REGION := util.GetEnv("AWS_S3_REGION")
+	BUCKET_NAME := util.GetEnv("AWS_S3_BUCKET_NAME")
 
 	configS3 := &aws.Config{
 		Region:      aws.String(REGION),
@@ -24,7 +25,7 @@ func UploadToS3(c echo.Context, filename string, src multipart.File) (string, er
 	s3Session := session.New(configS3)
 	uploader := s3manager.NewUploader(s3Session)
 	result, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String("mycuanbucket"),
+		Bucket: aws.String(BUCKET_NAME),
 		Key:    aws.String("img/"+ filename),
 		Body:   src,
 	})
