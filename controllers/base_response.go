9673ff375fe10d96bc81e9ac4b130e71e48d93b1
Zenk41 <ardhidhani@gmail.com>
2022-12-02 04:14:16 +0700

    feat: add response fail

--- controllers/base_response.go
+++ controllers/base_response.go
@@ -8,10 +8,22 @@ type Response[T any] struct {
 	Data    T      `json:"data"`
 }
 
+type ResponseFail struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 func NewResponse[T any](c echo.Context, statusCode int, statusMessage string, message string, data T) error {
 	return c.JSON(statusCode, Response[T] {
 		Status: statusMessage,
 		Message: message,
 		Data: data,
+	})
+}
+
+func NewResponseFail(c echo.Context, statusCode int, statusMessage string, message string) error {
+	return c.JSON(statusCode, ResponseFail {
+		Status: statusMessage,
+		Message: message,
 	})
 }
