ca1b4d58accf9718bbf5b31d5cae8f5414349eb2
Zenk41 <ardhidhani@gmail.com>
2022-12-04 04:45:06 +0700

    fix : Image not required in update

--- controllers/users/request/json.go
+++ controllers/users/request/json.go
@@ -25,7 +25,7 @@ type UpdatePassword struct {
 }
 
 type UpdateData struct {
-	Image       string `json:"image" form:"image" validate:"required"`
+	Image       string `json:"image" form:"image"`
 	Name        string `json:"name" form:"name" validate:"required"`
 	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required"`
 	Email       string `json:"email" form:"email" validate:"required,email"`
