62e0e5df588e0d302d14d6201b8668780cc51adb
aldimas07 <aldimasjaya.07@gmail.com>
2022-12-16 17:48:55 +0700

    creating domain and usecase

--- businesses/vouchers/domain.go
+++ businesses/vouchers/domain.go
@@ -7,12 +7,39 @@ import (
 )
 
 type Voucher struct {
-  ID int ``
-  Code string ``
-  
-  CreatedAt time.Time `json:"created_at"`
-  ExpiredAt time.Time `json:"expire_at,omitempty"`
-  UpdatedAt time.Time`json:"updated_at"`
-  DeletedAt gorm.DeletedAt`json:"deleted_at"`
+	ID                  uint           `json:"id"`
+	Voucher_Code        string         `gorm:"NOT NULL; UNIQUE_INDEX " json:"voucher_code"`
+	Voucher_Description string         `json:"voucher_description"`
+	Voucher_Balance     int            `json:"voucher_balance"`
+	Quantity_Stock      int            `json:"quantity_stock"`
+	CreatedAt           time.Time      `json:"created_at"`
+	ExpiredAt           string         `json:"expired_at"`
+	UpdatedAt           time.Time      `json:"updated_at"`
+	DeletedAt           gorm.DeletedAt `json:"deleted_at"`
 }
 
+type VoucherInputCreate struct {
+	Voucher_Code        string `gorm:"NOT NULL; UNIQUE_INDEX " json:"voucher_code"`
+	Voucher_Description string `json:"voucher_description"`
+	Voucher_Balance     int    `json:"voucher_balance"`
+	Quantity_Stock      int    `json:"quantity_stock"`
+	ExpiredAt           string `json:"expired_at"`
+}
+
+type Usecase interface {
+	Create(voucherDomain *VoucherInputCreate) VoucherInputCreate
+	Update(voucherDomain *Voucher) error
+	Delete(voucherID int) error
+	GetByCode(voucherCode string) (*Voucher, error)
+	GetAll(voucherID int) []Voucher
+	UseVoucher(voucherCode string) (*Voucher, error)
+}
+
+type Repository interface {
+	Create(voucherDomain *VoucherInputCreate) VoucherInputCreate
+	Update(voucherDomain *Voucher) error
+	Delete(voucherID int) error
+	GetAll(voucherID int) []Voucher
+	GetByCode(voucherCode string) (*Voucher, error)
+	UseVoucher(voucherCode string) (*Voucher, error)
+}
--- businesses/vouchers/usecase.go
+++ businesses/vouchers/usecase.go
@@ -1,1 +1,37 @@
 package vouchers
+
+
+
+type voucherUsecase struct {
+	voucherRepository Repository
+}
+
+func NewVoucherUsecase(voucherRepo Repository) Usecase {
+	return &voucherUsecase{
+		voucherRepository: voucherRepo,
+	}
+}
+
+func (vu *voucherUsecase) Create(voucherDomain *VoucherInputCreate) VoucherInputCreate {
+	return vu.voucherRepository.Create(voucherDomain)
+}
+
+func (vu *voucherUsecase) Update(voucherDomain *Voucher) error {
+	return vu.voucherRepository.Update(voucherDomain)
+}
+
+func (vu *voucherUsecase) Delete(voucherID int) error {
+	return vu.voucherRepository.Delete(voucherID)
+}
+
+func (vu *voucherUsecase) GetAll(voucherID int) []Voucher {
+	return vu.voucherRepository.GetAll(voucherID)
+}
+
+func (vu *voucherUsecase) GetByCode(voucherCode string) (*Voucher, error) {
+	return vu.voucherRepository.GetByCode(voucherCode)
+}
+
+func (vu *voucherUsecase) UseVoucher(voucherCode string) (*Voucher, error) {
+	return vu.voucherRepository.UseVoucher(voucherCode)
+}
